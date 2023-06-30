3ba6e45bb76c26020d1488eeae0fc7bb9d94dae0
Udo Kohlmeyer <udo@vmare.com>
2023-06-30 15:18:32 +0200

    SDGF-108: Update dependencies and add Javadoc and sources

--- build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/dependency-constraints/src/main/groovy/com/vmware/gemfire/gradle/plugins/DependencyConstraints.groovy
@@ -23,10 +23,10 @@ class DependencyConstraints {
     depVersionMapping.put("antlrVersion", "2.7.7")
     depVersionMapping.put("findbugsVersion", "3.0.2")
     depVersionMapping.put("multithreadedtcVersion", "1.01")
-    depVersionMapping.put("springBootVersion", "2.7.10")
+    depVersionMapping.put("springBootVersion", "2.7.13")
     depVersionMapping.put("springShellVersion", "1.2.0.RELEASE")
-    depVersionMapping.put("springFrameworkVersion", "5.3.26")
-    depVersionMapping.put("testcontainersVersion", "1.17.5")
+    depVersionMapping.put("springFrameworkVersion", "5.3.28")
+    depVersionMapping.put("testcontainersVersion", "1.17.6")
 
     return depVersionMapping
   }
