009226e1a9dd6a45de3ab3f10953dfae64df8033
Udo Kohlmeyer <udo@vmware.com>
2024-01-20 09:43:35 +1100

    SDGF-191: Remove platform-constraints plugin

--- spring-gemfire-starter/build.gradle.kts
+++ spring-gemfire-starter/build.gradle.kts
@@ -25,7 +25,7 @@ publishingDetails {
 
 dependencies {
   implementation(platform(bom.spring.framework.bom))
-  implementation(platform(bom.spring.boot.dependencies.bom))
+  api(platform(bom.spring.boot.dependencies.bom))
   implementation(platform(bom.spring.security.bom))
   implementation(platform(bom.testcontainers.dependencies.bom))
   api("org.springframework.boot:spring-boot-starter")
