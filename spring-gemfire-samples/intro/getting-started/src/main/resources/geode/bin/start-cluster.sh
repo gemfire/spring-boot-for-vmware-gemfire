933b774fcecad435b8b4484774c27e8080af7d93
Patrick Johnson <jpatrick@vmware.com>
2024-03-20 10:13:09 -0700

    SDGF-196: update copyright

--- spring-gemfire-samples/intro/getting-started/src/main/resources/geode/bin/start-cluster.sh
+++ spring-gemfire-samples/intro/getting-started/src/main/resources/geode/bin/start-cluster.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#
+# Copyright 2024 Broadcom. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
 gfsh -e "run --file=@samples-dir@/intro/getting-started/src/main/resources/geode/bin/start-cluster.gfsh"
