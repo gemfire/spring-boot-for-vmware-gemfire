a452490a9279f106a5299ff586a0db90dd456730
Patrick Johnson <jpatrick@vmware.com>
2024-03-20 11:07:25 -0700

    SDGF-196: update copyright

--- spring-gemfire-samples/caching/inline/src/main/resources/schema.sql
+++ spring-gemfire-samples/caching/inline/src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2024 Broadcom. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 CREATE TABLE IF NOT EXISTS calculations (
 	operand INTEGER NOT NULL,
   	operator VARCHAR(256) NOT NULL,
