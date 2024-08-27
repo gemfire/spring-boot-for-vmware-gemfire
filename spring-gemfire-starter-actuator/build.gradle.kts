0384eedfd9dc6f6aac92e28cabb579e999ab11f4
Udo Kohlmeyer <udo@vmware.com>
2024-08-27 21:57:47 +0200

    SDGF-294: Add publishing to Artifactory

--- spring-gemfire-starter-actuator/build.gradle.kts
+++ spring-gemfire-starter-actuator/build.gradle.kts
@@ -4,23 +4,24 @@
  */
 
 plugins {
-    id("project-base")
-    id("gemfire-repo-artifact-publishing")
+  id("project-base")
+  id("gemfire-repo-artifact-publishing")
+  id("gemfire-artifactory")
 }
 
 description = "Spring Boot Actuator Starter for VMware GemFire"
 
 publishingDetails {
-    artifactName.set("spring-boot-actuator-2.7-gemfire-${ProjectUtils.getGemFireBaseVersion(property("gemfireVersion").toString())}")
-    longName.set(project.description)
-    description.set(project.description)
+  artifactName.set("spring-boot-actuator-2.7-gemfire-${ProjectUtils.getGemFireBaseVersion(property("gemfireVersion").toString())}")
+  longName.set(project.description)
+  description.set(project.description)
 }
 
 dependencies {
-    api(platform(bom.spring.framework.bom))
-    api(platform(bom.spring.boot.dependencies.bom))
-    api(platform(bom.spring.security.bom))
-    api(platform(bom.testcontainers.dependencies.bom))
-    api(project(":spring-gemfire-starter"))
-    api(project(":spring-gemfire-actuator-autoconfigure"))
+  api(platform(bom.spring.framework.bom))
+  api(platform(bom.spring.boot.dependencies.bom))
+  api(platform(bom.spring.security.bom))
+  api(platform(bom.testcontainers.dependencies.bom))
+  api(project(":spring-gemfire-starter"))
+  api(project(":spring-gemfire-actuator-autoconfigure"))
 }
