19b684307fbef8a8b263494e5b90e3e81a93fae0
Udo Kohlmeyer <udo@vmware.com>
2024-08-28 19:11:51 +0200

    SDGF-294: Add publishing to Artifactory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -47,6 +47,20 @@ include("spring-geode-tests:smoke-tests:mock-session-caching")
 include("spring-geode-tests:smoke-tests:multi-store")
 include("spring-geode-tests:smoke-tests:peer-cache-application")
 
+project(":spring-gemfire").name = "spring-gemfire"
+project(":spring-gemfire-jetty11").name = "spring-gemfire-jetty11"
+project(":spring-gemfire-autoconfigure").name = "spring-gemfire-autoconfigure"
+project(":spring-gemfire-extensions").name = "spring-gemfire-extensions"
+
+project(":spring-gemfire-starter").name = "spring-gemfire-starter"
+project(":spring-gemfire-actuator").name = "spring-gemfire-actuator"
+project(":spring-gemfire-actuator-autoconfigure").name = "spring-gemfire-actuator-autoconfigure"
+project(":spring-gemfire-starter-logging").name = "spring-gemfire-starter-logging"
+project(":spring-gemfire-starter-session").name = "spring-gemfire-starter-session"
+project(":spring-gemfire-starter-actuator").name = "spring-gemfire-starter-actuator"
+project(":spring-gemfire-starter-test").name = "spring-gemfire-starter-test"
+
+
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
