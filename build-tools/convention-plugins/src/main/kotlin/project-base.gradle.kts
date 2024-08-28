956b8de28c117720d8430003fc9a153c41df6184
Udo Kohlmeyer <udo@vmware.com>
2024-08-28 19:11:51 +0200

    SDGF-294: Add publishing to Artifactory

--- build-tools/convention-plugins/src/main/kotlin/project-base.gradle.kts
+++ build-tools/convention-plugins/src/main/kotlin/project-base.gradle.kts
@@ -7,6 +7,7 @@ plugins {
   id("java-library")
   id("idea")
   id("eclipse")
+  id("commercial-repositories")
 }
 
 group = "com.vmware.gemfire"
@@ -28,13 +29,6 @@ dependencies{
 
 repositories {
   mavenCentral()
-  maven {
-    credentials {
-      username = property("gemfireRepoUsername") as String
-      password = property("gemfireRepoPassword") as String
-    }
-    url = uri("https://commercial-repo.pivotal.io/data3/gemfire-release-repo/gemfire")
-  }
   val additionalMavenRepoURLs = project.findProperty("additionalMavenRepoURLs").toString()
   if (!additionalMavenRepoURLs.isNullOrBlank() && additionalMavenRepoURLs.isNotEmpty()) {
     additionalMavenRepoURLs.split(",").forEach {
@@ -47,10 +41,6 @@ repositories {
 
 configurations.all {
   resolutionStrategy.cacheChangingModulesFor(0, "minutes")
-}
-
-tasks.withType<JavaCompile>().configureEach {
-  options.compilerArgs.add("-parameters")
 }
 
 fun getGemFireBaseVersion(): String {
