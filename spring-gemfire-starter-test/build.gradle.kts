209cc4bcb7066016639367eec96c83c5fb0d7e90
Udo Kohlmeyer <udo@vmware.com>
2024-09-20 09:15:22 +1000

    SDGF-286: update patch versions SDGF-294: parameterize artifactory URL

--- spring-gemfire-starter-test/build.gradle.kts
+++ spring-gemfire-starter-test/build.gradle.kts
@@ -12,9 +12,6 @@ description = "Spring Boot Test Starter for VMware GemFire"
 
 
 dependencies {
-	implementation(platform(bom.spring.framework.bom))
-	implementation(platform(bom.spring.boot.dependencies.bom))
-	implementation(platform(bom.spring.security.bom))
 	implementation(platform(bom.testcontainers.dependencies.bom))
 	api(project(":spring-gemfire-starter"))
 	api("org.springframework.boot:spring-boot-starter-test")
