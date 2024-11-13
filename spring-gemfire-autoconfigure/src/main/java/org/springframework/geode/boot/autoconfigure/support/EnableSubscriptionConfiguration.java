7c2e1e23bdd05729ed1f64960c55ca0ec883cd56
Patrick Johnson <patrick.johnson@broadcom.com>
2024-11-12 16:21:36 -0800

    SDGF-8090: Allow overriding of subscription-enabled on the default pool.

--- spring-gemfire-autoconfigure/src/main/java/org/springframework/geode/boot/autoconfigure/support/EnableSubscriptionConfiguration.java
+++ spring-gemfire-autoconfigure/src/main/java/org/springframework/geode/boot/autoconfigure/support/EnableSubscriptionConfiguration.java
@@ -4,6 +4,8 @@
  */
 package org.springframework.geode.boot.autoconfigure.support;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
@@ -11,8 +13,10 @@ import org.apache.geode.cache.client.Pool;
 
 import org.apache.shiro.util.CollectionUtils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.gemfire.config.annotation.ClientCacheConfigurer;
 import org.springframework.data.gemfire.config.annotation.PoolConfigurer;
 
@@ -32,6 +36,12 @@ import org.springframework.data.gemfire.config.annotation.PoolConfigurer;
 @SuppressWarnings("unused")
 public class EnableSubscriptionConfiguration {
 
+	protected static final String SPRING_DATA_GEMFIRE_PROPERTY_PREFIX = "spring.data.gemfire";
+	protected static final String UNNAMED_POOL_PROPERTY = "spring.data.gemfire.pool.subscription-enabled";
+
+	@Autowired
+	protected Environment environment;
+
 	private static final String DEFAULT_POOL_NAME = "DEFAULT";
 	private static final String GEMFIRE_POOL_NAME = "gemfirePool";
 
@@ -39,7 +49,11 @@ public class EnableSubscriptionConfiguration {
 
 	@Bean
 	public ClientCacheConfigurer enableSubscriptionClientCacheConfigurer() {
-		return (beanName, clientCacheFactoryBean) -> clientCacheFactoryBean.setSubscriptionEnabled(true);
+		return (beanName, clientCacheFactoryBean) -> {
+			if(!subscriptionsDisabledByProperty(POOL_NAMES)) {
+				clientCacheFactoryBean.setSubscriptionEnabled(true);
+			}
+		};
 	}
 
 	@Bean
@@ -47,6 +61,31 @@ public class EnableSubscriptionConfiguration {
 
 		return (beanName, poolFactoryBean) -> Optional.ofNullable(beanName)
 			.filter(POOL_NAMES::contains)
-			.ifPresent(poolName -> poolFactoryBean.setSubscriptionEnabled(true));
+			.ifPresent(poolName -> {
+				if(!subscriptionsDisabledByProperty(beanName)) {
+					poolFactoryBean.setSubscriptionEnabled(true);
+				}
+			});
+	}
+
+	private boolean subscriptionsDisabledByProperty(Collection<String> propertyNames) {
+		if(environment == null) {
+			return false;
+		}
+		for (String poolName : propertyNames) {
+			String namedPoolProperty = getNamedPoolProperty(DEFAULT_POOL_NAME, "subscription-enabled");
+			if(Boolean.FALSE.equals(environment.getProperty(namedPoolProperty, Boolean.class))) {
+				return true;
+			}
+		}
+		return Boolean.FALSE.equals(environment.getProperty(UNNAMED_POOL_PROPERTY, Boolean.class));
+	}
+
+	private boolean subscriptionsDisabledByProperty(String propertyName) {
+		return subscriptionsDisabledByProperty(Collections.singleton(propertyName));
+	}
+
+	private String getNamedPoolProperty(String poolName, String suffix) {
+		return String.format("%1$s.%2$s.%3$s.%4$s", SPRING_DATA_GEMFIRE_PROPERTY_PREFIX, "pool", poolName, suffix);
 	}
 }
